d3086fd4ba813e3097eb10ba6c8450fcde732d5f
gitName <a@b.c>
2021-06-23 13:54:20 +0530

    Increasing Order Search tree solution added.

--- Recursion/Leetcode/_897_IncreasingOrderSearchTree.cs
+++ Recursion/Leetcode/_897_IncreasingOrderSearchTree.cs
@@ -1,20 +1,29 @@
-﻿namespace Recursion.Leetcode
+﻿using System.Collections.Generic;
+
+namespace Recursion.Leetcode
 {
     class _897_IncreasingOrderSearchTree
     {
-        private static TreeNode baseRoot;
         public TreeNode IncreasingBST(TreeNode root)
         {
-            if(root == null)
+            List<int> vals = new();
+            Inorder(root, ref vals);
+            TreeNode ans = new TreeNode(0);
+            TreeNode current = ans;
+            foreach(var item in vals)
             {
-                baseRoot = new TreeNode();
-                return baseRoot;
+                current.right = new TreeNode(item);
+                current = current.right;
             }
-            
-            var node = IncreasingBST(root.left);
-            node.right = new TreeNode { val=  root.val};
-            IncreasingBST(root.right);
-            return node;
+            return ans.right;
+        }
+
+        public void Inorder(TreeNode node, ref List<int> vals)
+        {
+            if (node == null) return;
+            Inorder(node.left, ref vals);
+            vals.Add(node.val);
+            Inorder(node.right, ref vals);
         }
     }
 }
